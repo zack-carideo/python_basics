af2bb2f544ff2dbb0efab6cb357aaee880e87706
zack-carideo <zjc1002@gmail.com>
2021-12-10 13:03:31 -0500

    added notes

--- generators.py
+++ generators.py
@@ -1,5 +1,12 @@
 """
 GENERATORS
+    - Generators simplify creation of iterables. Anything that can be done with a generator can be done by implementing iterator protocol. 
+    - Generators require lesser lines of code than solving the problem with iterators and iterables.
+    - Generators are functions having an yield keyword. Any function which has “yield” in it is a generator.
+    - Calling a generator function creates an iterable. Since it is an iterable so it can be used with iter() and with a for loop.
+    - Generator takes care of creating the iterable. It also takes care of creating the underlying iterator. And next() of this iterator() is such that 
+    it returns each ‘yield’ value of generator one after the other. When there is no more ‘yield’ in the generator function then this iterator raises StopIteration.
+
 """
 
 
